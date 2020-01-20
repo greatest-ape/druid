9c47e42656628547f1fd46845829f370c740cde1
Colin Rofls <colin@cmyr.net>
2020-01-20 14:41:32 -0500

    Make old_data &T instead of Option<&T>

--- druid/src/widget/container.rs
+++ druid/src/widget/container.rs
@@ -72,7 +72,7 @@ impl<T: Data> Widget<T> for Container<T> {
         self.inner.event(ctx, event, data, env);
     }
 
-    fn update(&mut self, ctx: &mut UpdateCtx, _old_data: Option<&T>, data: &T, env: &Env) {
+    fn update(&mut self, ctx: &mut UpdateCtx, _old_data: &T, data: &T, env: &Env) {
         self.inner.update(ctx, data, env);
     }
 
